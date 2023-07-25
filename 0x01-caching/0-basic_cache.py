b77c83a5350219b379aa938c448260309e6712e5
Ngece <ngecemlamli@gmail.com>
2023-07-26 00:15:35 +0200

    committing basic cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -10,13 +10,6 @@ class BasicCache():
         """
         self.cache_data = {}
 
-    def print_cache(self):
-        """ Print the cache
-        """
-        print("Current cache:")
-        for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key, self.cache_data.get(key)))
-
     def put(self, key, item):
         """ Add an item in the cache
         """
