3f710a9b2b376944fd9c805c0a08fe6411c78881
Ngece <ngecemlamli@gmail.com>
2023-07-26 23:39:56 +0200

    committing LIFO cache

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -4,20 +4,31 @@
 from base_caching import BaseCaching
 
 class LIFOCache(BaseCaching):
+    """ LIFO caching system """
     def __init__(self):
-        """ Initialize class instance. """
+        """ Initiliaze
+        """
         super().__init__()
+        self.queue = []
 
     def put(self, key, item):
-        """ Add an item in the cache """
-        if key and item:
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            pass
+        else:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                if key not in self.cache_data.keys():
+                    discard = self.queue.pop()
+                    del self.cache_data[discard]
+                    print("DISCARD: {}".format(discard))
+            self.queue.append(key)
             self.cache_data[key] = item
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            self.cache_data.pop(self.last_item)
-            print('DISCARD:', self.last_item)
-        if key:
-            self.last_item = key
 
     def get(self, key):
-        """ Get an item by key """
-        return self.cache_data.get(key)
+        """ Get an item by key
+        """
+        if key is None or key not in self.cache_data.keys():
+            return None
+        else:
+            return self.cache_data[key]
